b6011d9d2202385f3b42289c34d1d85f5a9f232e
EnzoXavierPessoa <xavierenzo@gmail.com>
2026-03-18 16:50:08 -0300

    feat: Urna e Procedimento

---
+++ procedimento/procedimento.js
@@ -0,0 +1,25 @@
+function procedimento()
+    
+function inicio(){
+
+    
+     inicio();
+    
+     function mensagem(frase){
+        let linha = "-";
+        let i = 0;
+        let frase = "Boas vindas!";
+    
+        do{
+           linha = linha + "-"
+           i++;      
+    }while( i < 50 );
+   
+     alert( linha + "\n" + frase + "\n" + linha);
+     
+     } comium
+
+     inicio()
+
+}
+
